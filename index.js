4efdbc3d35faceb37f6b2c688810c10153695a92
Amy Dalziel <amy.dalziel@keyin.com>
2024-08-05 22:15:32 -0230

    POST method working

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ document.getElementById('fetchData').addEventListener('click', () => {
         })
         .catch(error => console.error('Error:', error));
 });
+
+document.getElementById('postNewBST').addEventListener('click', () => {
+
+    const input = document.getElementById('nodeValues').value;
+
+    const intValues = input.split(',').map(v => parseInt(v.trim())); 
+    console.log(intValues); 
+
+    
+    fetch('http://localhost:8080/newBinarySearchTree', { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(intValues)
+    })
+    .then(response => response.json())
+    .then(result => {
+        document.getElementById('result').textContent = JSON.stringify(result, null, 2);
+    })
+    .catch(error => console.error('Error:', error));
+});
